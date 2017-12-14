ffe16ae5526849e96eb97a8e54a1b7e903140043
xianjiezh <904292766@qq.com>
2017-12-14 11:21:40 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'vue-resume/dist',
     proxyTable: {},
 
     // Various Dev Server settings
